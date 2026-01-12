b917eb8d42af252c30f6ffa5a78b5f5c792b06c9
yoon92kr <yoon92_kr@naver.com>
2026-01-12 17:02:05 +0900

    Cloudflare 연동을 위한 재설정

--- onestarpark/next.config.ts
+++ onestarpark/next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
